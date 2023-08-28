1c74110db686071b5c157a09b6498b1ba2f0c91c
andriyanovme <mathewsandrianoff@yandex.ru>
2023-08-28 16:20:11 +0300

    Закончил "Навигацию".

---
+++ ImageFeed/SingleImage/SingleImageViewController.swift
@@ -0,0 +1,29 @@
+//
+//  SingleImageViewController.swift
+//  ImageFeed
+//
+//  Created by E on 23.08.2023.
+//
+
+import UIKit
+
+final class SingleImageViewController: UIViewController {
+    var image: UIImage! {
+        didSet {
+            guard isViewLoaded else { return }
+            imageView.image = image
+        }
+    }
+    
+    @IBOutlet var imageView: UIImageView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        imageView.image = image
+    }
+    
+    
+    @IBAction func didTapBackButton() {
+        dismiss(animated: true, completion: nil)
+    }
+}
