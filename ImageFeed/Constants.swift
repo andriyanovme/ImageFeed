9146af1ef0060a65e0e494b157635425b723c333
andriyanovme <mathewsandrianoff@yandex.ru>
2023-09-18 15:39:51 +0300

    Начал новый спринт

---
+++ ImageFeed/Constants.swift
@@ -0,0 +1,14 @@
+//
+//  Constants.swift
+//  ImageFeed
+//
+//  Created by E on 15.09.2023.
+//
+
+import Foundation
+
+let AccessKey = "f0CZLHXydVPLKRS5sX4lEsvyHLQ6z3QsdOF-qkeiUnA"
+let SecretKey = "XhYYYASqjiObm88bvEDKLLarF4owPdeKpp1c5zPVqGw"
+let RedirectURI = "urn:ietf:wg:oauth:2.0:oob"
+let AccessScope = "public+read_user+write_likes"
+let DefaultBaseURL = URL(string: "https://api.unsplash.com")!
